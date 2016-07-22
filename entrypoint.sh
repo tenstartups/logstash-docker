106b02eb0e21296905b5e59dee64ca0989e58d42
Marc Lennox <marc.lennox@gmail.com>
2016-07-22 11:55:14 -0400

    Removed unused code

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,6 @@ set -e
 # from the `logstash` base layer, but runs Logstash as root, not the `logstash`
 # user, to ensure access to the journal.
 
-set -e
-
-# Add logstash as command if needed
-if [[ "$1" == -* ]]; then
-  set -- logstash "$@"
-fi
-
-exec "$@"
-
 # Add logstash as command if needed
 if [ "${1:0:1}" = '-' ]; then
 	set -- logstash "$@"
